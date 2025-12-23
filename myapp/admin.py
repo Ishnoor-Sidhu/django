76049b8cde093dab01cbc117cb846ddd4643ca31
Ishnoor-Sidhu <ishnoorsidhu.sidhu736@gmail.com>
2025-12-23 15:39:44 +0530

    database registared

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Contact,Registeration,Login
 
 # Register your models here.
+admin.site.register(Contact)
+admin.site.register(Registeration)
+admin.site.register(Login)
